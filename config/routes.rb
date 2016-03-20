84fc7a761a7bf239f7945ed2fc2e4a9120de5588
Alexander Huang <alexander95015@gmail.com>
2016-03-20 16:23:48 +0800

    further fixes for assets

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       post 'search'
     end
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
